2674479aa7a92c12c2d22007bca8dcebe6370a0e
Mike Sly <ec2-user@ip-172-31-32-105.us-west-2.compute.internal>
2015-04-08 17:27:10 +0000

    nodeserver updates

--- nodeserver.js
+++ nodeserver.js
@@ -3,4 +3,21 @@ var app = express();
 app.use('/', express.static('./html')).
     use('/images', express.static( '../images')).
     use('/lib', express.static( '../lib'));
+app.get('/gettime', function (req, res) {
+    console.log("in gettime route");
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect("mongodb://localhost/project", function(err, db){
+	if(err) throw err;
+	db.collection("times",function(err, times){
+	    if(err) throw err;
+	    times.find(function(err,items){
+		items.toArray(function(err, itemArr){
+		    res.writeHead(200);
+		    res.end(JSON.stringify(itemArr));
+		});
+	    });
+	});
+    }); 
+});
 app.listen(80);
+
